9759d6f7cfae7e68b8f767a8f3495f6ddc944007
duanhongyi <duanhongyi@doopai.com>
2022-09-15 20:58:28 +0800

    fix(rabbitmq): image tag error

--- deploy.mk
+++ deploy.mk
@@ -1,8 +1,8 @@
 install:
-	helm upgrade rabbitmq charts/rabbitmq --install --namespace drycc --set rabbitmq.org=${IMAGE_PREFIX},rabbitmq.image_tag=${VERSION}
+	helm upgrade rabbitmq charts/rabbitmq --install --namespace drycc --set rabbitmq.org=${IMAGE_PREFIX},rabbitmq.imageTag=${VERSION}
 
 upgrade: 
-	helm upgrade rabbitmq charts/rabbitmq --namespace drycc --set rabbitmq.org=${IMAGE_PREFIX},rabbitmq.image_tag=${VERSION}
+	helm upgrade rabbitmq charts/rabbitmq --namespace drycc --set rabbitmq.org=${IMAGE_PREFIX},rabbitmq.imageTag=${VERSION}
 
 uninstall:
 	helm delete rabbitmq
